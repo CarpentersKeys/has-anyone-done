04dbbfd558da7c5f905d51147a0a68dfb567127e
joshua <carpenterskeys@gmail.com>
2022-05-22 12:37:51 -0400

    fixed crossOrigin attritbute and cleared testing js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=Grape+Nuts&display=swap" rel="stylesheet" />
                 <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
             </Head>
