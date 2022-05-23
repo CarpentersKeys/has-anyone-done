cc3cb3c97fa437b3dec7a6e5ad3309aad68c5d14
joshua <carpenterskeys@gmail.com>
2022-05-23 13:48:45 -0400

    feature: admin page to view all entries, api endpoint, moved submit entry into admin

---
+++ pages/admin/view-entries.js
@@ -0,0 +1,65 @@
+import TimedMessage from "../../Components/TimedMessage";
+import { useSession } from "next-auth/react";
+import styles from '../../styles/ViewEntries.module.css';
+import { useState, useEffect } from "react";
+import ListPageButtons from "../../Components/ListPageButtons";
+
+export default function ViewEntries() {
+    const { data: session } = useSession();
+    const [entries, entriesSet] = useState([]);
+    const [page, pageSet] = useState(0);
+    useEffect(() => {
+        console.log(page)
+    }, [page])
+
+    useEffect(() => {
+        const url = '/api/view'
+        const reqOptions = {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+
+        fetch(url, reqOptions)
+            .then(result => result.ok && result.json() || new Error('result not ok'))
+            .then(json => entriesSet(json.entries))
+            .catch(err => console.log('fetch error in ViewEntries', err))
+    }, [])
+
+    if (session) {
+
+        return (entries.length > 0 &&
+            <div className={styles.container}>
+                <h1 id={styles.title}>these Have Been Done..</h1 >
+                <ul className={styles.listCont}>
+                    {
+                        entries.slice(page * 10, page * 10 + 10).map((e, i) => (
+                            <li key={i}>
+                                <p>
+                                    {e.text} | {e.updatedAt.match(/[\d]{4}-[\d]{2}-[\d]{2}/)}
+                                </p>
+                            </li>)
+                        )
+                    }
+                </ul>
+                <ListPageButtons entries={new Array(21)} pageSet={pageSet} />
+            </div >
+        )
+    } else {
+        return (
+            <TimedMessage id={styles.TimedMessage} jsx={
+                <div className={styles.container}>
+                    <h3 className={styles.message}>
+                        Not signed in
+                    </h3>
+                    <p className={styles.message}>btw only I can sign in</p>
+                    <div className={styles.auth}>
+                        <button onClick={() => signIn()}>Sign In</button>
+                    </div>
+                </div>
+            } time='800' />
+        )
+    }
+}
