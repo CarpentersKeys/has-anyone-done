960cb4bfac7d73cbaac3a0766f115957f880c5d2
joshua <carpenterskeys@gmail.com>
2022-05-28 21:46:54 -0400

    improved submission handling, added temp display of submission status,

--- pages/admin/submit-entry.js
+++ pages/admin/submit-entry.js
@@ -1,34 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSession } from 'next-auth/react';
 import styles from '../../styles/Submit-Entry.module.css';
 import TimedMessage from "../../Components/TimedMessage";
 import SignInOrOut from "../../Components/SignInOrOut";
 import { useRouter } from "next/router";
+import NotSignedIn from "../../Components/NotSignedIn";
 
 export default function SubmitEntry() {
     const [entryText, entryTextSet] = useState('');
+    const [submitDisplay, submitDisplaySet] = useState(null);
     const { data: session } = useSession();
     const router = useRouter();
 
+    // handle timed submission messages (display success or fail for 3s)
+    useEffect(() => {
+        let timer;
+
+        if (submitDisplay) {
+            timer = setTimeout(() => { submitDisplaySet(null) }, 3000)
+        };
+
+        return () => { timer && clearTimeout(timer); };
+    }, [submitDisplay]);
+
     async function handleSubmit(evt) {
+        // TODO: learn about input sanitization 
         evt.preventDefault();
 
         const fetchUrl = `../api/entry`
-        console.log('fetchurl', fetchUrl)
-        const reqOptions = {
+        const postOptions = {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                text: entryText,
+                text: entryText.trim(),
             })
         }
 
-        const result = await fetch(fetchUrl, reqOptions);
+        const result = await fetch(fetchUrl, postOptions);
         if (result.ok) {
+            submitDisplaySet('Entry successfully submote!');
+            entryTextSet('');
+            const recentText = await result.json();
+            session.recentEntryText = recentText;
             router.push('/admin/view-entries');
+        } else {
+            submitDisplaySet('Entry failfully submote!');
         }
     }
 
@@ -47,20 +66,16 @@ export default function SubmitEntry() {
                     />
                     {/* FUTURE: auto search the db and display similar options as input to avoid dups */}
                 </form>
+                {submitDisplay && <span>{submitDisplay}</span>}
                 <SignInOrOut />
             </div >
         )
     } else {
         return (
-            <TimedMessage jsx={
-                <div className={styles.container}>
-                    <h3 className={styles.message}>
-                        Not signed in
-                    </h3>
-                    <p className={styles.message}>btw only I can sign in</p>
-                    <SignInOrOut />
-                </div>
-            } time='800' />
+            <div>
+                <NotSignedIn />
+                <SignInOrOut />
+            </div>
         )
     }
 }
